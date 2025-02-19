7f56145c9e11d900b3f56b70b3fcff1e6d9af2a1
Even Richardson <121592609+evenmillz@users.noreply.github.com>
2025-02-18 20:08:42 -0800

    Updated webpages
    + Incorporated smooth scrolling to all of my pages where applicable

--- Projects/Grace-Hopper/JS/grace.js
+++ Projects/Grace-Hopper/JS/grace.js
@@ -5,18 +5,20 @@ const closePopup = document.getElementById("closePopupForm");
 
 // This opens the popup when the button is clicked
 contactButton.addEventListener("click", () => {
-    popupForm.style.display = "block";
-    
+    popupForm.style.display = "flex";
+    setTimeout(() => popupForm.classList.add("show"), 50);
 });
 
 // This closes the popup when the close button is clicked
 closePopup.addEventListener("click", () => {
-    popupForm.style.display = "none";
+    popupForm.style.display = "show";
+    setTimeout(() => popupForm.style.display = "none", 500);
 });
 
 // This closes the popup when the area outside of the form is clicked
 window.addEventListener("click", (e) => {
     if (e.target === popupForm) {
-        popupForm.style.display = "none";
+        popupForm.classList.remove("show");
+        setTimeout(() => popupForm.style.display = "none", 500);
     }
 });
